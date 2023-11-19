46b95458b117169d05d23dca8bd053670c27990a
unknown <zjnkill18@gmail.com>
2023-11-19 17:15:37 +0700

    222

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='min-h-screen flex justify-center items-center'>
+    <div className='min-h-screen relative flex justify-center items-center  bg-nft-black-4 overflow-hidden'>
+       <div className='absolute min-w-[600px] min-h-[600px] md:min-w-[300px] md:min-h-[300px]  -top-40 -left-40 bg-nft-black-1 rounded-full'/>
+       <div className='absolute min-w-[600px] min-h-[600px]  md:min-w-[300px] md:min-h-[300px] -bottom-40 -right-40 bg-nft-black-1 rounded-full'/>
+
         <SignIn 
         afterSignInUrl={'/client'} 
         afterSignUpUrl={'/client'} 
--- components/LeftSidebar/LeftSideBar.tsx
+++ components/LeftSidebar/LeftSideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LeftSideBar = () => {
   return (
-    <div className='min-h-screen z-20 bg-nft-gray-0 dark:bg-nft-black-2 text-white min-w-256 md:hidden border-r dark:border-gray-700 border-gray-300'>
+    <div className='min-h-screen z-20 bg-nft-gray-0 dark:bg-nft-black-2 text-white min-w-256 md:hidden border-r dark:border-nft-black-4 border-gray-300'>
       
     </div>
   )
