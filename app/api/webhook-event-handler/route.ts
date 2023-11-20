522cc66cc4a1c47169011d76fb5fd1b1b20d43b2
unknown <zjnkill18@gmail.com>
2023-11-20 22:20:26 +0700

    22

--- app/api/webhook-event-handler/route.ts
+++ app/api/webhook-event-handler/route.ts
@@ -1,19 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
 
-export  function POST(req:Request) {
-    // let error = req.body.error
-    // console.log(req)
-    // if (error != 0) {
-    //     //Không làm gì cả.
-    //     console.log(error)
-    //     return;
-    // }
+export  function POST(req:NextApiRequest,res:NextApiResponse) {
+    let error = req.body.error;
+    if (error != 0) {
+        //Không làm gì cả.
+        return;
+    }
     
-    // //mảng chứa danh sách các giao dịch
-    // let transactions = req.body.data;
+    //mảng chứa danh sách các giao dịch
+    let transactions = req.body.data;
     
-    // console.log(`Received ${transactions.length} transactions`);
+    console.log(`Received ${transactions.length} transactions`);
     
     //thêm code xử lý giao dịch ở đây.
     
-    Response.json({ message: 'Hello from Next.js!' })
+    res.end("OK");
 }
