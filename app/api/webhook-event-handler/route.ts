54b53330e0b7be0c7e42935d621819bcf1aad507
unknown <zjnkill18@gmail.com>
2023-11-20 22:08:27 +0700

    2

--- app/api/webhook-event-handler/route.ts
+++ app/api/webhook-event-handler/route.ts
@@ -1,20 +1,19 @@
-import { NextApiRequest, NextApiResponse } from "next";
 
-export async function POST(req:NextApiRequest,res:NextApiResponse<any>) {
-    let error = req.body.error
-    console.log(req)
-    if (error != 0) {
-        //Không làm gì cả.
-        console.log(error)
-        return;
-    }
+export  function POST(req:Request) {
+    // let error = req.body.error
+    // console.log(req)
+    // if (error != 0) {
+    //     //Không làm gì cả.
+    //     console.log(error)
+    //     return;
+    // }
     
-    //mảng chứa danh sách các giao dịch
-    let transactions = req.body.data;
+    // //mảng chứa danh sách các giao dịch
+    // let transactions = req.body.data;
     
-    console.log(`Received ${transactions.length} transactions`);
+    // console.log(`Received ${transactions.length} transactions`);
     
     //thêm code xử lý giao dịch ở đây.
     
-    return Response.json({ messsage:"hello word "})
+    Response.json({ message: 'Hello from Next.js!' })
 }
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // "/" will be accessible to all users
   publicRoutes: ["/", "/api/webhook","/sign-in","/api/handler-bank-transfer","/api/register-webhook","/api/user-paid","/api/webhook-event-handler"],
-  ignoredRoutes: ["/api/webhook", "api/chatgpt","/api/webhook-event-handler"],
+  ignoredRoutes: ["/api/webhook", "api/chatgpt","/api/webhook-event-handler","/api/handler-bank-transfer","/api/register-webhook","/api/user-paid"],
 
 });
 ;
